6152c60375681eb2760a81eb0d43d6e5c7ce6ee7
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2014-03-06 17:09:38 +0900

    upgrade to 0.10.0 alpha

--- www/py-graphite-web/files/patch-setup.py
+++ www/py-graphite-web/files/patch-setup.py
@@ -1,25 +1,11 @@
---- setup.py.ori	2012-06-21 15:38:54.044808453 -0700
-+++ setup.py	2012-06-21 15:41:16.067809618 -0700
-@@ -15,11 +15,11 @@
+--- ./setup.py.orig	2014-03-01 14:14:49.000000000 +0900
++++ ./setup.py	2014-03-06 11:58:13.000000000 +0900
+@@ -16,7 +16,7 @@
  storage_dirs = []
  
- for subdir in ('whisper', 'rrd', 'log', 'log/webapp'):
+ for subdir in ('whisper', 'ceres', 'rrd', 'log', 'log/webapp'):
 -  storage_dirs.append( ('storage/%s' % subdir, []) )
-+  storage_dirs.append( ('graphite/storage/%s' % subdir, []) )
++  storage_dirs.append( ('%%GRAPHITE_STORAGE%%/%s' % subdir, []) )
  
- webapp_content = {}
+ webapp_content = defaultdict(list)
  
--for root, dirs, files in os.walk('webapp/content'):
-+for root, dirs, files in os.walk('graphite/webapp/content'):
-   for filename in files:
-     filepath = os.path.join(root, filename)
- 
-@@ -29,7 +29,7 @@
-     webapp_content[root].append(filepath)
- 
- 
--conf_files = [ ('conf', glob('conf/*.example')) ]
-+conf_files = [ ('/usr/local/etc/graphite', glob('conf/*.example')) ]
- examples = [ ('examples', glob('examples/example-*')) ]
- 
- setup(
