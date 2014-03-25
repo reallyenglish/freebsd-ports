aaa9cc2dd5c4a7bfe35f2368bfcb895658792834
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2014-03-25 16:19:40 +0900

    rm bak and orig files created by do-patch
    - fix another linuxism

--- www/py-graphite-web/files/patch-bin__build-index.sh
+++ www/py-graphite-web/files/patch-bin__build-index.sh
@@ -1,7 +1,8 @@
 --- ./bin/build-index.sh.orig	2014-03-01 14:14:49.000000000 +0900
-+++ ./bin/build-index.sh	2014-03-06 11:58:13.000000000 +0900
++++ ./bin/build-index.sh	2014-03-18 15:29:02.000000000 +0900
 @@ -1,3 +1,3 @@
- #!/bin/bash
+-#!/bin/bash
 -export PYTHONPATH="/opt/graphite/webapp/:$PYTHONPATH"
++#!/bin/sh
 +export PYTHONPATH="%%GRAPHITE_WEBAPP_ROOT%%:$PYTHONPATH"
  ./build-index
